3090750df82ea8bbc38729fa0e1aab2ffc5200ff
jossmulalinda <josstugass@gmail.com>
2025-09-30 18:19:02 +0900

    Menambahkan folder tugas

---
+++ bin/Tugas 2/Latihan_2.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  // Menyimpan panjang dan lebar dalam variabel double
+  double panjang = 10;
+  double lebar = 5;
+
+  // Rumus untuk menghitung luas dan keliling
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  // Berikut ini untuk menampilkan hasil
+  print("Panjang = $panjang");
+  print("Lebar = $lebar");
+  print("Luas = $luas");
+  print("Keliling = $keliling");
+}
